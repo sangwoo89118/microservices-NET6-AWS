ac2d9a74eff4fcf57c53f8090d68320c387b088f
sangwoo <sangwoo89118@gmail.com>
2022-05-14 19:24:30 -0700

    add initial deps

--- CloudWeather.Precipitation/Program.cs
+++ CloudWeather.Precipitation/Program.cs
@@ -1,5 +1,11 @@
+using Microsoft.AspNetCore.Mvc;
+
 var builder = WebApplication.CreateBuilder(args);
 
 var app = builder.Build();
+
+app.MapGet("/observation/{zip}", (string zip, [FromQuery] int? days) => { 
+    return Results.Ok(zip);
+});
 
 app.Run();
